17dc98aee5f327ecd1d33f801dac125fbb10815a
Hannes Blitza <blitza@terrestris.de>
2024-02-07 08:48:00 +0100

    fix: remove unneeded assets

--- masterportal-config/start-config/config.js
+++ masterportal-config/start-config/config.js
@@ -1,8 +1,5 @@
 // eslint-disable-next-line no-unused-vars
 const Config = {
-    alerting: {
-        fetchBroadcastUrl: "./resources/newsFeedPortalAlerts.json"
-    },
     namedProjections: [
         ["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
     ],
